43c3a94bb91ca612b7f91f810f5a1c65c8f8fffb
Dxxxxxxww <gotu2018@163.com>
2021-04-27 17:43:50 +0800

    添加手写 reduce by dxw

--- write.js
+++ write.js
@@ -1,50 +1,12 @@
-function filterAsyncRoutes(menus, parentAction = '') {
-  const res = []
-
-  menus.forEach((menu) => {
-    // const component = parentAction
-    //   ? () => import(`@/views/${parentAction}/${menu.popedomAction}/InfoMng`)
-    //   : Layout
-    const tmp = {
-      path: menu.popedomAction,
-      name: menu.popedomAction.slice(1),
-      // component,
-    }
-    if (menu.children?.length) {
-      tmp.children = filterAsyncRoutes(menu.children)
-    }
-    res.push(tmp)
-  })
-
-  return res
-}
-
-const asyncRoutes = [
-  {
-    popedomAction: '/systemManage', // 一级菜单
-    popedomIconcls: 'xitong', //  一级菜单icon
-    popedomId: '01',
-    popedomName: '系统管理', // 一级菜单名称
-    popedomPid: '0',
-    children: [
-      // 二级菜单
-      {
-        popedomAction: '/organization', // 二级菜单
-        popedomId: '0101',
-        popedomName: '机构信息管理', // 二级菜单名称
-        popedomPid: '01',
-        popedomButtons: ['010101', '010102'], //  当前用户拥有的按钮权限
-      },
-      {
-        popedomAction: '/organization',
-        popedomId: '0101',
-        popedomName: '机构信息管理',
-        popedomPid: '01',
-        popedomButtons: [],
-      },
-    ],
-  },
-]
-
-const abc = filterAsyncRoutes(asyncRoutes)
-console.log(abc)
+Array.prototype.reduce =
+  Array.prototype.reduce ||
+  function r2(callback, initVal) {
+    const arr = this
+    const isHasInitVal = arguments.length > 1
+    let base = isHasInitVal ? initVal : arr[0]
+    const startIndex = isHasInitVal ? 0 : 1
+    arr.slice(startIndex).forEach((val, index) => {
+      base = callback(base, val, index + startIndex, arr)
+    })
+    return base
+  }
