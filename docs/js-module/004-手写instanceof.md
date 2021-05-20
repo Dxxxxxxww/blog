2b7109a9273a40c7c64252959c327703a77ecff6
Dxxxxxxww <gotu2018@163.com>
2021-05-20 09:39:23 +0800

    增加学习记录相关 by dxw

---
+++ docs/js-module/004-手写instanceof.md
@@ -0,0 +1,28 @@
+# 手写之四 instanceof
+
+## ◆ instanceof
+```js
+function instanceofMock(l, r) {
+  // 如果是基本类型直接返回 false
+  if (typeof l !== 'object') {
+    return false
+  }
+  while (true) {
+    // 原型链到头了
+    if (l === null) {
+      return false
+    }
+    if (l === r.prototype) {
+      return true
+    }
+    l = l.__proto__
+  }
+}
+
+instanceofMock('', String) // false
+
+function Person() {}
+const p = new Person()
+
+instanceofMock(p, Person) // true
+```
--- test.js
+++ test.js
@@ -1,13 +1,69 @@
-Array.prototype.reduce =
-  Array.prototype.reduce ||
-  function r2(callback, initVal) {
-    const arr = this
-    const isHasInitVal = arguments.length > 1
-    let base = isHasInitVal ? initVal : arr[0]
-    const startIndex = isHasInitVal ? 0 : 1
-    arr.slice(startIndex).forEach((val, index) => {
-      base = callback(base, val, index + startIndex, arr)
+function red() {
+  console.log('red')
+}
+function green() {
+  console.log('green')
+}
+function yellow() {
+  console.log('yellow')
+}
+
+function p(t, cb) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      cb()
+      resolve()
+    }, t)
+  })
+}
+// 这里需要返回 p(1000, green) 函数执行，而不是返回 undefined 是因为需要等待相应的秒数后再执行 resolve （pending->resolved）
+// 如果返回 undefined then 所返回的新的 promise 就已经是 resolved 状态了，
+// new Promise(resolve=>{
+//   return resolve(1)
+// }).then(res=>{
+//   console.log(res)
+// }).then(res=>{
+//   console.log(res)
+//   return undefined
+// }).then(res=>{
+//   console.log(res)
+// }) 输出 1 undefined undefined  Promise {<fulfilled>: undefined}
+
+const step = () => {
+  p(3000, red)
+    .then(() => {
+      return p(1000, green)
+      // return undefined
     })
-    return base
-  }
-// its a test
+    .then(() => {
+      return p(2000, yellow)
+      // return undefined
+    })
+    .then(step)
+}
+step()
+
+// const task = (timer, light) =>
+//     new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (light === 'red') {
+//                 red()
+//             }
+//             else if (light === 'green') {
+//                 green()
+//             }
+//             else if (light === 'yellow') {
+//                 yellow()
+//             }
+//             resolve()
+//         }, timer)
+//     })
+
+// const step = () => {
+//     task(3000, 'red')
+//         .then(() => task(1000, 'green'))
+//         .then(() => task(2000, 'yellow'))
+//         .then(step)
+// }
+
+// step()
