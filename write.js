fcd79a7712025e3feae2e4937a8b4ad3cfa2683e
dxw <gotu2018@163.com>
2021-10-11 21:34:36 +0800

    feat: 增加异步 by dxw

--- write.js
+++ write.js
@@ -1,48 +1,12 @@
-const flowRight = (...args) => (value) =>
-  args.reverse().reduce((initVal, current) => current(initVal), value)
+const getInfo = axios('/api/getInfo')
+const timeout = new Promise((_, reject) => {
+  setTimeout(() => reject(new Error('timeout')), 5000)
+})
 
-class IO {
-  static of(value) {
-    return new IO(function() {
-      return value
-    })
-  }
-
-  constructor(fn) {
-    this._value = fn
-  }
-
-  map(fn) {
-    return new IO(flowRight(fn, this._value))
-  }
-
-  join() {
-    return this._value()
-  }
-
-  // 组合函数
-  flatMap(fn) {
-    return this.map(fn).join()
-  }
-}
-
-const fa = () => {
-  return new IO(() => {
-    console.log('fa')
-    return 1
+Promise.race([getInfo, timeout])
+  .then((val) => {
+    console.log(val)
   })
-}
-
-const fb = (x) => {
-  return new IO(() => {
-    console.log('fb')
-    return x
+  .catch((err) => {
+    console.log(err)
   })
-}
-
-const res = fa()
-  .map((x) => x + 2) // 这里可以嵌套许多 map 函数，因为最终都会合并成一个函数
-  .flatMap(fb)
-  .join()
-
-console.log(res)
