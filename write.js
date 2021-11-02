90a522b4a082bc0b91dffb8bd706c5de30dca141
dxw <gotu2018@163.com>
2021-11-02 21:35:34 +0800

    feat: 提交笔记若干

--- write.js
+++ write.js
@@ -10,65 +10,15 @@
 // 常见的微任务有 Promise.then , MutationObserver(监听dom变化的事件), node环境下的 process.nextTick
 // 除了上面的微任务以外的异步任务都是宏任务，比如 setTimeout, setInterval,输入输出操作,事件等。
 
-// const PENDING = "pending";
-// const FULFILLED = "fulfilled";
-// const REJECTED = "rejected";
+const obj = {
+  store: ['a', 'b', 'c'],
+  [Symbol.iterator]: function*() {
+    for (const item of this.store) {
+      yield item
+    }
+  },
+}
 
-// class MyPromise {
-//     status = PENDING;
-//     value = undefined;
-//     reason = undefined;
-//     // 同一个 promise 对象可以调用多个 then 方法(不是链式调用)，所以使用函数保存
-//     handleSuccess = [];
-//     handleFail = [];
-
-//     constructor(callback) {
-//         callback(this.resolve, this.reject);
-//     }
-
-//     resolve = (value) => {
-//         if (this.status !== PENDING) return;
-//         this.value = value;
-//         this.status = FULFILLED;
-//         while (this.handleSuccess.length) {
-//             // 当异步完成时执行处理函数
-//             this.handleSuccess.shift()(value);
-//         }
-//     };
-//     reject = (reason) => {
-//         if (this.status !== PENDING) return;
-//         this.reason = reason;
-//         this.status = REJECTED;
-//         while (this.handleFail.length) {
-//             // 当异步完成时执行处理函数
-//             this.handleFail.shift()(reason);
-//         }
-//     };
-//     then(success, fail) {
-//         return new MyPromise((resolve, reject) => {
-//             if (this.status === FULFILLED) {
-//                 resolve(success(this.value));
-//             } else if (this.status === REJECTED) {
-//                 reject(fail(this.reason));
-//             } else {
-//                 // 异步时先将处理函数保存下来
-//                 success && this.handleSuccess.push(success);
-//                 fail && this.handleFail.push(fail);
-//             }
-//         });
-//     }
-// }
-
-// const p = new MyPromise((resolve, reject) => {
-//     setTimeout(() => resolve("成功"), 2000);
-//     // resolve('成功')
-// });
-
-// const p2 =p.then(function a(v) {
-//     console.log(123,v);
-//     return 100;
-// })
-
-// p2.then(function b(v) {
-//     console.log(456,v);
-// });
+for (const it of obj) {
+  console.log('娃哈哈', it)
+}
