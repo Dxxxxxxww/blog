679ebc750d9f137a84491f754e96e8121f61423a
dxw <gotu2018@163.com>
2021-11-03 20:33:09 +0800

    feat: 增加闭包出入栈

--- test2.js
+++ test2.js
@@ -1,21 +1,12 @@
-function* createIdMaker() {
-  let i = 1
-  while (true) {
-    yield i++
+function sayHi() {
+  debugger
+  const hi = 'hi'
+  return function son() {
+    debugger
+    console.log(hi)
   }
 }
-const sayHi = () => {
-  let str = 'hello'
-  console.log(str)
-}
 
-const c = createIdMaker()
-console.log(c.next())
-console.log(c.next())
-// 这里执行完后，generator 函数会出栈，sayHi 会入栈。
-sayHi()
-console.log(c.next())
-// 也就是说 generator 每 next 一次都会入栈执行，然后遇到 yield 暂停再出栈
-sayHi()
-console.log(c.next())
-console.log(c.next())
+const gSon = sayHi()
+
+gSon()
