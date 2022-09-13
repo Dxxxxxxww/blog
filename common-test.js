df3b6bfc2ed201760578110b884fcc194d240e8d
dxx <gotu2018@163.com>
2022-09-13 21:11:52 +0800

    feat: 复习3道算法题，增加左旋字符串，编写了一点点监控项目

--- common-test.js
+++ common-test.js
@@ -1,18 +1,17 @@
 function quickSort(list, i, j) {
   if (i > j) return
   let l = i,
-    r = j
-  const item = list[i]
+    r = j,
+    item = list[l]
   while (l < r) {
-    // 从右往左找，找到比目标值小的，移动到左侧
+    // 从右往左找
     while (l < r && item < list[r]) {
       r--
     }
-    // 找到了对应的  r 放到目标值的左侧，并且 l下标 移动到下一个值
     if (l < r) {
       list[l++] = list[r]
     }
-    // 从左往右找，找到比目标值大的，移动到右侧
+    // 从左往右找
     while (l < r && item > list[l]) {
       l++
     }
@@ -23,6 +22,7 @@ function quickSort(list, i, j) {
   list[l] = item
   quickSort(list, i, l - 1)
   quickSort(list, l + 1, j)
+  return list
 }
 
 const list = []
@@ -31,5 +31,4 @@ for (let i = 0; i < 10; i++) {
 }
 
 console.log('list: ', list)
-quickSort(list, 0, 9)
-console.log(list)
+console.log(quickSort(list, 0, 9))
